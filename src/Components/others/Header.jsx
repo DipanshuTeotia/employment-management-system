de12c16e2618dc5c1102362e1ade36009c8df387
dipanshu teotia <dipanshuteotia007@gmail.com>
2026-09-09 00:58:15 +0530

    logout functionality done

--- src/Components/others/Header.jsx
+++ src/Components/others/Header.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Header = ({data}) => {
   // console.log(data)
+  const logOut = ()=>{
+    localStorage.setItem("loggedInUser","");
+    window.location.reload();
+  }
   return (
     <div>
       <div className='flex justify-between items-center p-4'>
         <h1 className='text-3xl font-bold'>Hello,<br/> <span>username</span></h1>
-        <button className='bg-red-500 p-1 rounded outline-none'>Log out</button>
+        <button onClick={logOut} className='bg-red-500 p-1 rounded outline-none'>Log out</button>
       </div>
     </div>
   )
