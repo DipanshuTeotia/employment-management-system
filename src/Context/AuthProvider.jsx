1c83881da8b54d08fa84e106f6cd043716141990
dipanshu teotia <dipanshuteotia007@gmail.com>
2026-09-10 03:22:09 +0530

    final touch up done

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -5,7 +5,7 @@ export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
   const [userData, setUserData] = useState([]);
-  localStorage.clear();
+  // localStorage.clear();
   useEffect(() => {
     // setLocalStorage();
     const {employees,admin} = getLocalStorage();
